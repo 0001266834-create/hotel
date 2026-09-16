2af01454248c46baab9477b9bc22f6cee88eb197
Daniel <0001266834@senaimgaluno.com.br>
2026-09-15 22:06:28 -0300

    ultima aula

--- salvar_cliente.php
+++ salvar_cliente.php
@@ -3,12 +3,13 @@
 require_once "conexao.php";
 
 $nome = $_POST['nome'];
-$email =$_POST['email'];
-$telefone =$_POST['telefone'];
-$senha =$_POST['senha'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$senha = $_POST['senha'];
+$estrelas = $_POST['estrelas'];
 
 $sql = "INSERT INTO clientes
- (nome,email,telefone,senha) VALUES ('$nome', '$email', '$telefone', '$senha')";
+ (nome,email,telefone,senha,estrelas) VALUES ('$nome', '$email', '$telefone', '$senha','$estrelas')";
 
 if(mysqli_query($conexao, $sql)){
   header("Location: minhas_reservas.php");
--- salvar_hotel.php
+++ salvar_hotel.php
@@ -3,9 +3,14 @@
 
 $nome_hotel = $_POST['nome_hotel'];
 $cidade = $_POST['cidade'];
+$E_mail = $_POST['E_mail'];
+$senha = $_POST['Senha'];
 $estrelas = $_POST['estrelas'];
 
-$sql = "INSERT INTO hoteis (nome,cidade,estrelas) VALUES ('$nome_hotel', '$cidade', $estrelas)";
+
+$estrelas = $_POST['estrelas'];
+
+$sql = "INSERT INTO hoteis (nome,cidade,email,senha,estrelas) VALUES ('$nome_hotel', '$cidade','$E_mail','$Senha', $estrelas)";
 
 if(mysqli_query($conexao, $sql)){
   header("Location: minhas_reservas.php");
